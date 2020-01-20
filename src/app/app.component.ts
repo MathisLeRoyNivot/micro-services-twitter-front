17aeddd297e1938ab5b2a32881af726589d82feb
mathislrn <mathis.lrn061198@gmail.com>
2020-01-20 15:01:23 +0100

    Adding navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<app-navbar></app-navbar>'
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'micro-services-twitter-front';
