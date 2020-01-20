69529b465a166a016820cfd52d88835947ff8e8a
leorigaudeau <leo.rigaudeau@ynov.com>
2020-01-20 15:39:56 +0100

    ajout du component btn-microphone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MicroButtonComponent } from './micro-button/micro-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MicroButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
