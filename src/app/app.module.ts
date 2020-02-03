2e0b1fd7c09851d5a6559ee3f1c1ecfda3e1f4e4
leorigaudeau <leo.rigaudeau@ynov.com>
2020-02-04 00:04:50 +0100

    Feat(textArea)+Fix(bugbtnSTT)et plein d'autre truc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContainerLeftComponent } from './container-left/container-left.componen
 import { MicroButtonComponent } from './micro-button/micro-button.component';
 import { AudioComponent } from './audio/audio.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TextFieldComponent } from './text-field/text-field.component';
 // import { SpeechRecognition } from "nativescript-speech-recognition";
 
 @NgModule({
@@ -15,7 +16,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     ContainerLeftComponent,
     MicroButtonComponent,
     AudioComponent,
-    NavbarComponent
+    NavbarComponent,
+    TextFieldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/text-field/text-field.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+
+// import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-text-field",
+  templateUrl: "./text-field.component.html",
+  styleUrls: ["./text-field.component.scss"]
+})
+export class TextFieldComponent implements OnInit {
+  // public http: HttpClient;
+  // constructor(private httpR: HttpClient) {
+    // this.http = httpR;
+  // }
+
+  ngOnInit() {}
+  click(txt) {
+    console.log(txt.value);
+    if (txt.value) {
+      //TODO A tester Imperativement apres merge
+    // this.http
+    //   .post('http://localhost:3000/api/twitter/status' , {
+    //     status:txt.value
+    //   }).toPromise().then((res)=>{
+    //     alert('Vous venez de tweeter : ' + txt.value)
+    // txt.value=""
+    //   }).catch((err)=>{
+    //     alert(err)
+    //   });
+    
+    }
+    
+    
+  }
+  
+  enter(btn) {
+    btn.classList.add("animated", "heartBeat");
+  }
+  leave(btn) {
+    btn.classList.remove("animated", "heartBeat");
+  }
+}
